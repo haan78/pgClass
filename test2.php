36321fcb5c244f65427905cff418dbf974f2b11c
Ali Barış Öztürk <haan78@gmail.com>
2020-09-20 12:57:55 +0300

    k

--- test2.php
+++ test2.php
@@ -12,5 +12,20 @@ echo $time->format('H:i');
 
 
 
-echo replaceQ("Ali?Barış?Öztürk",[ "1", "2", "3"]);
+//echo replaceQ("Ali?Barış?Öztürk",[ "1", "2", "3"]);
 //echo str_replace(["?","?"],[1,2],"Ali ? ? Veli");
+
+$subject = "offset12";
+$num = 0;
+if (preg_match('/^offset(\d+)$/', $subject, $matches)) {
+    $num = $matches[1];
+} elseif ($subject=="next") {
+    $num +=1;
+} elseif ($subject=="previous") {
+    if ($num > 1) {
+        $num += -1;
+    }
+} elseif ($subject == "first") {
+    $num = 1;
+}
+echo $num;
